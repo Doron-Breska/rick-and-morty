7ad876a839d58473b06671ebf5c9b07bd92daaa7
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-04-26 13:47:50 +0200

    styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer({ nextPage, prevPage, num }) {
   return (
     <footer className="Footer">
-      <button onClick={prevPage} className="prevPage" type="button">
+      <button onClick={prevPage} className="prevPage page-btn" type="button">
         &larr;
       </button>
-      <span>{num}</span>
-      <button onClick={nextPage} className="nextPage" type="button">
+      <span className="page-btn">{num}</span>
+      <button onClick={nextPage} className="nextPage page-btn" type="button">
         &rarr;
       </button>
     </footer>
